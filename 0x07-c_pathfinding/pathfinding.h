3df89e2c1aeca1b970457c7689a9e0fc350c4e42
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-02-27 11:57:15 -0800

    Add header file, first commit

---
+++ 0x07-c_pathfinding/pathfinding.h
@@ -0,0 +1,14 @@
+#ifndef PATHFINDING
+#define PATHFINDING
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+#endif /* PATHFINDING */
